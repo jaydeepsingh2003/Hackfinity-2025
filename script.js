a9ae30085dc65f6f1cdd3f1ff0078791e273bcaf
Jaydeep <someone@email.com>
2025-07-25 14:51:58 +0530

    commit

--- script.js
+++ script.js
@@ -8,27 +8,29 @@
   document.querySelectorAll('.fade-in-section').forEach(section => {
     observer.observe(section);
   });
-  const countdown = () => {
-    const target = new Date("2025-05-05T00:00:00").getTime();
+  // const countdown = () => {
+  //   const target = new Date("2025-05-05T00:00:00").getTime();
 
-    const now = new Date().getTime();
-    const gap = target - now;
+  //   const now = new Date().getTime();
+  //   const gap = target - now;
   
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+  //   const second = 1000;
+  //   const minute = second * 60;
+  //   const hour = minute * 60;
+  //   const day = hour * 24;
   
-    const d = Math.floor(gap / day);
-    const h = Math.floor((gap % day) / hour);
-    const m = Math.floor((gap % hour) / minute);
-    const s = Math.floor((gap % minute) / second);
+ /* The commented out code block is calculating the remaining time gap between the current time and a
+ target date in days, hours, minutes, and seconds. */
+    // const d = Math.floor(gap / day);
+    // const h = Math.floor((gap % day) / hour);
+    // const m = Math.floor((gap % hour) / minute);
+    // const s = Math.floor((gap % minute) / second);
   
-    document.getElementById("days").innerText = d;
-    document.getElementById("hours").innerText = h;
-    document.getElementById("minutes").innerText = m;
-    document.getElementById("seconds").innerText = s;
-  };
+  //   document.getElementById("days").innerText = d;
+  //   document.getElementById("hours").innerText = h;
+  //   document.getElementById("minutes").innerText = m;
+  //   document.getElementById("seconds").innerText = s;
+  // };
   
   setInterval(countdown, 1000);
 
